edb8caabd1c6dc2b8af41ffe80042025e4e1b611
t0j08e3 <taylor.jones1@walmart.com>
2023-02-19 21:22:13 -0600

    setup of jest

--- lint-staged.js
+++ lint-staged.js
@@ -1,7 +1,6 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'eslint --max-warnings=5',
-        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
     ],
     '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
